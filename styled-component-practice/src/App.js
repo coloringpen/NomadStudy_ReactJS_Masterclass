b1ef6edcd3145bcfc1f015a1916f667011a19070
coloringpen <illangayo00@gmail.com>
2023-05-30 23:58:50 +0900

    Introducing themeprovider

--- styled-component-practice/src/App.js
+++ styled-component-practice/src/App.js
@@ -1,10 +1,24 @@
-import FirstSC from "./styled_components/FirstSC";
+import styled from "styled-components";
+
+// theme provider로 감싸진 component들은 모두 theme에 접근 가능해짐!
+const Title = styled.h1`
+	color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	height: 100vh;
+	width: 100vw;
+	justify-content: center;
+	align-items: center;
+	background-color: ${(props) => props.theme.backgroundColor};
+`;
 
 function App() {
 	return (
-		<>
-			<FirstSC />
-		</>
+		<Wrapper>
+			<Title>Hello</Title>
+		</Wrapper>
 	);
 }
 
--- styled-component-practice/src/index.js
+++ styled-component-practice/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+	textColor: "whitesmoke",
+	backgroundColor: "#111",
+};
+
+const lightTheme = {
+	textColor: "#111",
+	backgroundColor: "whitesmoke",
+};
+// 각각의 theme의 key들의 이름은 같아야 한다.
+// 그래야 theme만 갈아끼워도 component의 변형없이 그대로 style을 적용할 수 있으므로
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={lightTheme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
