114723b039f9ead372055dee90757ebebf3494a4
coloringpen <illangayo00@gmail.com>
2023-06-02 02:49:41 +0900

    error element

--- router-practice/src/Router.tsx
+++ router-practice/src/Router.tsx
@@ -2,14 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import Home from "./screens/Home";
 import About from "./screens/About";
+import NotFound from "./screens/NotFound";
+import ComponentError from "./screens/ComponentError";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
 		children: [
-			{ path: "", element: <Home /> },
+			{
+				path: "",
+				element: <Home />,
+				errorElement: <ComponentError />,
+			},
 			{ path: "/about", element: <About /> },
 		],
+		errorElement: <NotFound />,
 	},
 ]);
