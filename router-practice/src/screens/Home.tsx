5ad648f61ed70b0ee1f9fa7dd39ffcaf3e1b2768
coloringpen <illangayo00@gmail.com>
2023-06-03 02:24:09 +0900

    useSearchParams

--- router-practice/src/screens/Home.tsx
+++ router-practice/src/screens/Home.tsx
@@ -1,16 +1,22 @@
 import { users } from "../db";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 export default function Home() {
+	const [readSearchParams, setSearchParams] = useSearchParams();
+	setTimeout(() => {
+		setSearchParams({
+			day: "today",
+			tomorrow: "123",
+		});
+	});
+
 	return (
 		<div>
 			<h1>Users</h1>
 			<ul>
 				{users.map((user) => (
 					<li key={user.id}>
-						<Link to={`/users/${user.id}`}>
-							{user.name}
-						</Link>
+						<Link to={`/users/${user.id}`}>{user.name}</Link>
 					</li>
 				))}
 			</ul>
