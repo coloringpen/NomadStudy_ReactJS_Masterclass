e5c24fd2da80da80a6be23339b8a0b28fc5e9900
coloringpen <illangayo00@gmail.com>
2023-06-09 23:59:37 +0900

    interface

--- cointracker/src/screens/Coin.tsx
+++ cointracker/src/screens/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { Container, Header, Title, Loader } from "../styles/CoinStyles";
 
@@ -10,13 +10,31 @@ export default function Coin() {
 	const { coinID } = useParams();
 	const [isLoading, setIsLoading] = useState(true);
 	const state = useLocation().state as IRouteState;
-	console.log(state.name);
-	console.log(coinID);
+	const [coinDetailInfo, setCoinDetailInfo] = useState({});
+	const [coinPriceInfo, setCoinPriceInfo] = useState({});
+
+	useEffect(() => {
+		(async () => {
+			const coinDetailData = await (
+				await fetch(`https://api.coinpaprika.com/v1/coins/${coinID}`)
+			).json();
+
+			const coinPriceData = await (
+				await fetch(`https://api.coinpaprika.com/v1/tickers/${coinID}`)
+			).json();
+			setCoinDetailInfo(coinDetailData);
+			setCoinPriceInfo(coinPriceData);
+			setIsLoading(false);
+			console.log(coinDetailData);
+			console.log(coinPriceData);
+		})();
+	}, []);
 	return (
 		<>
 			<Container>
 				<Header>
 					<Title>{state?.name || "Loading"}</Title>
+					{/** state가 있으면 .name 가져오고, 아니면 "Loading을 가져오기" */}
 				</Header>
 
 				{isLoading ? <Loader>on loading process</Loader> : null}
