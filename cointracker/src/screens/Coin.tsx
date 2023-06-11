bf61a46705354d6860e8d401cc7f982bc720662a
coloringpen <illangayo00@gmail.com>
2023-06-11 19:45:29 +0900

    api type interface

--- cointracker/src/screens/Coin.tsx
+++ cointracker/src/screens/Coin.tsx
@@ -6,12 +6,68 @@ interface IRouteState {
 	name: string;
 }
 
+interface IInfoData {
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	is_new: boolean;
+	is_active: boolean;
+	type: string;
+	logo: string;
+	description: string;
+	message: string;
+	open_source: boolean;
+	started_at: string;
+	development_status: string;
+	hardware_wallet: boolean;
+	proof_type: string;
+	org_structure: string;
+	hash_algorithm: string;
+	first_data_at: string;
+	last_data_at: string;
+}
+
+interface IPriceData {
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	circulating_supply: number;
+	total_supply: number;
+	max_supply: number;
+	beta_value: number;
+	first_data_at: string;
+	last_updated: string;
+	quotes: {
+		USD: {
+			ath_date: string;
+			ath_price: number;
+			market_cap: number;
+			market_cap_change_24h: number;
+			percent_change_1h: number;
+			percent_change_1y: number;
+			percent_change_6h: number;
+			percent_change_7d: number;
+			percent_change_12h: number;
+			percent_change_15m: number;
+			percent_change_24h: number;
+			percent_change_30d: number;
+			percent_change_30m: number;
+			percent_from_price_ath: number;
+			price: number;
+			volume_24h: number;
+			volume_24h_change_24h: number;
+		};
+	};
+}
+
 export default function Coin() {
 	const { coinID } = useParams();
-	const [isLoading, setIsLoading] = useState(true);
 	const state = useLocation().state as IRouteState;
-	const [coinDetailInfo, setCoinDetailInfo] = useState({});
-	const [coinPriceInfo, setCoinPriceInfo] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
+	const [coinDetailInfo, setCoinDetailInfo] = useState<IInfoData>();
+	const [coinPriceInfo, setCoinPriceInfo] = useState<IPriceData>();
 
 	useEffect(() => {
 		(async () => {
@@ -37,7 +93,7 @@ export default function Coin() {
 					{/** state가 있으면 .name 가져오고, 아니면 "Loading을 가져오기" */}
 				</Header>
 
-				{isLoading ? <Loader>on loading process</Loader> : null}
+				{isLoading ? <Loader>on loading process</Loader> : "nono"}
 			</Container>
 		</>
 	);
