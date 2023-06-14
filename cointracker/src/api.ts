f707deaa36b2e262a4bd2630ada4f3c0b4446afe
coloringpen <illangayo00@gmail.com>
2023-06-14 23:57:45 +0900

    fetcher func

--- cointracker/src/api.ts
+++ cointracker/src/api.ts
@@ -1,3 +1,13 @@
+const BASE_URL = "https://api.coinpaprika.com/v1/";
+
 export async function fetchCoins() {
-	return fetch("https://api.coinpaprika.com/v1/coins").then((response) => response.json());
+	return fetch(`${BASE_URL}coins`).then((response) => response.json());
+}
+
+export async function fetchCoinInfo(coinID: string) {
+	return fetch(`${BASE_URL}coins/${coinID}`).then((response) => response.json());
+}
+
+export async function fetchCoinTickers(coinID: string) {
+	return fetch(`${BASE_URL}tickers/${coinID}`).then((response) => response.json());
 }
