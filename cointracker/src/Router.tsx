3602504dd3ca0c010625094148f24449335cff81
coloringpen <illangayo00@gmail.com>
2023-06-14 23:59:38 +0900

    nested routes : chart and price

--- cointracker/src/Router.tsx
+++ cointracker/src/Router.tsx
@@ -2,6 +2,8 @@ import Root from "./Root";
 import { createBrowserRouter } from "react-router-dom";
 import Coins from "./screens/Coins";
 import Coin from "./screens/Coin";
+import Chart from "./components/Chart";
+import Price from "./components/Price";
 // tsx 파일로 설정해야 component, 즉 jsx 사용할 수 있음!!
 
 export const router = createBrowserRouter([
@@ -13,7 +15,20 @@ export const router = createBrowserRouter([
 				path: "",
 				element: <Coins />,
 			},
-			{ path: ":coinID", element: <Coin /> },
+			{
+				path: ":coinID",
+				element: <Coin />,
+				children: [
+					{
+						path: "chart",
+						element: <Chart />,
+					},
+					{
+						path: "price",
+						element: <Price />,
+					},
+				],
+			},
 		],
 	},
 ]);
