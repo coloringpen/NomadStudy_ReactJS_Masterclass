df36bd369c2d7e2fb7c2cfdc8ee725b7be1854f8
coloringpen <illangayo00@gmail.com>
2023-06-14 23:58:58 +0900

    react query dev tool adding

--- cointracker/src/Root.tsx
+++ cointracker/src/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { GlobalStyle } from "./styles/Global";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function Root() {
 	return (
 		<>
 			<GlobalStyle />
 			<Outlet />
+			<ReactQueryDevtools initialIsOpen={true} />
 		</>
 	);
 }
