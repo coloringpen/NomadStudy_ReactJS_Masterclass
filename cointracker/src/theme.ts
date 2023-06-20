90bf81cb94feaf08159d47c429fcfbe4adb338a6
coloringpen <illangayo00@gmail.com>
2023-06-20 23:43:31 +0900

    theme change

--- cointracker/src/theme.ts
+++ cointracker/src/theme.ts
@@ -1,13 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
-	bgColor: "#786fa6",
-	textColor: "#ED4C67",
-	accentColor: "#f5cd79",
+	bgColor: "#FFECEC",
+	textColor: "#1F6E8C",
+	accentColor: "#FF06B7",
+	pannelColor: "#73BBC9",
 };
 
 export const darkTheme: DefaultTheme = {
-	bgColor: "#090580",
-	textColor: "#E8A9A9",
-	accentColor: "#FCE9F1",
+	bgColor: "#27374D",
+	textColor: "#EEE2DE",
+	accentColor: "#FB2576",
+	pannelColor: "#569DAA",
 };
