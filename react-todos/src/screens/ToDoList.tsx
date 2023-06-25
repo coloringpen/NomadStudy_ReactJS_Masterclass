3e9c66fc6d144d2dbe844d509363fb11600af925
coloringpen <illangayo00@gmail.com>
2023-06-25 13:19:33 +0900

    add validation with register function

--- react-todos/src/screens/ToDoList.tsx
+++ react-todos/src/screens/ToDoList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { ButtonStyle, FormFrameStyle } from "../styles/componentStyles";
 
 // export default function ToDoList() {
 // 	const [newTodDo, setNewToDo] = useState("");
@@ -32,21 +33,29 @@ import { useForm } from "react-hook-form";
 // }
 
 export default function ToDoList() {
-	const { register, watch } = useForm();
-	console.log(watch());
+	const { register, handleSubmit } = useForm();
+	const onValid = (data: any) => {
+		console.log(data);
+	};
 
 	return (
 		<>
 			<div>
-				<form>
-					<input {...register("email")} placeholder="Email" />
-					<input {...register("firstName")} placeholder="First Name" />
-					<input {...register("lastName")} placeholder="Last Name" />
-					<input {...register("username")} placeholder="Username" />
-					<input {...register("pw")} placeholder="password" />
-					<input {...register("pwConfirm")} placeholder="password confirmation" />
-					<button>Add</button>
-				</form>
+				<FormFrameStyle onSubmit={handleSubmit(onValid)}>
+					<input {...register("email", { required: true })} placeholder="Email" />
+					<input
+						{...register("firstName", { required: true, minLength: 10 })}
+						placeholder="First Name"
+					/>
+					<input {...register("lastName", { required: true })} placeholder="Last Name" />
+					<input {...register("username", { required: true })} placeholder="Username" />
+					<input {...register("pw", { required: true })} placeholder="password" />
+					<input
+						{...register("pwConfirm", { required: true })}
+						placeholder="password confirmation"
+					/>
+					<ButtonStyle>Add</ButtonStyle>
+				</FormFrameStyle>
 			</div>
 		</>
 	);
