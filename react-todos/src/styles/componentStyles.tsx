3fa5e956e7e3ba62ab8a229c57b31af6713d517e
coloringpen <illangayo00@gmail.com>
2023-06-26 23:32:34 +0900

    style add

---
+++ react-todos/src/styles/componentStyles.tsx
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+export const FormFrameStyle = styled.form`
+	margin: 0 auto;
+	width: 500px;
+	display: flex;
+	flex-direction: column;
+	input {
+		width: 300px;
+		margin: 10px auto;
+	}
+`;
+
+export const ButtonStyle = styled.button`
+	width: 300px;
+	margin: 10px auto;
+`;
